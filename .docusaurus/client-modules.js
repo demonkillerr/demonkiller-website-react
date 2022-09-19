d008faae90524c57250b81347f9176f1f4b6a561
demonkiller <gaurangvishwakarma619@gmail.com>
2022-09-19 19:26:51 +0530

    remove .docusaarus cache

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/dk/code/01-webd/demonkiller-website-react/node_modules/infima/dist/css/default/default.css'),
-  require('/home/dk/code/01-webd/demonkiller-website-react/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/dk/code/01-webd/demonkiller-website-react/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/dk/code/01-webd/demonkiller-website-react/src/css/custom.css'),
-];
