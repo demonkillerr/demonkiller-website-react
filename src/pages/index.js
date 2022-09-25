c7f58ad0208ce7b315141860db9973c06670908c
demonkiller <gaurangvishwakarma619@gmail.com>
2022-09-25 23:22:59 +0530

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,16 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+
+
+const BLIND_75_URL =
+  'https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU';
+const BLIND_OFFER_NUMBERS_URL =
+  'https://www.teamblind.com/post/Sharing-my-offer-numbers-from-big-companies-for-your-reference-yNgqUPQR';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -26,14 +33,33 @@ function HomepageHeader() {
   );
 }
 
+function HeroSection() {
+  const {siteConfig = {}} = useDocusaurusContext();
+
+  return (
+    <header className={clsx('hero', styles.heroBanner)}>
+      <div className="container">
+        <img
+          alt={siteConfig.title}
+          className={clsx(styles.heroBannerLogo, 'margin-vert--md')}
+          src={useBaseUrl('img/logo.svg')}
+        />
+       
+        
+      </div>
+    </header>
+  );
+}
+
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      title={`Welcome from ${siteConfig.title}`}
+      description="DemonKiller's Portfolio & Project Documentation Website">
+      <HomepageHeader/>
       <main>
+        <HeroSection />
         <HomepageFeatures />
       </main>
     </Layout>
