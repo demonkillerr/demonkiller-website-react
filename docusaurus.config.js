43f33238dc0078bf2b947fc1df68ad3f70d0c31a
demonkiller <gaurangvishwakarma619@gmail.com>
2022-09-26 22:51:27 +0530

    add hide sidebar option

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       navbar: {
         title: 'DemonKiller',
         logo: {
