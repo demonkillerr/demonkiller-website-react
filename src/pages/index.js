a739fe7e68af3844b7216dc244afc5c8b55910a9
demonkiller <gaurangvishwakarma619@gmail.com>
2022-09-26 21:54:56 +0530

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,29 +9,30 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
 
-const BLIND_75_URL =
-  'https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU';
-const BLIND_OFFER_NUMBERS_URL =
-  'https://www.teamblind.com/post/Sharing-my-offer-numbers-from-big-companies-for-your-reference-yNgqUPQR';
+const ALG_URL =
+  'https://archlinuxgui.in/';
+const DK_YT_URL =
+  'https://www.youtube.com/c/demonkillerr';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/introduction">
-            Start Reading ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+//       <div className="container">
+//         <h1 className="hero__title">{siteConfig.title}</h1>
+//         <p className="hero__subtitle">{siteConfig.tagline}</p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--secondary button--lg"
+//             to="/docs/introduction">
+//             Start Reading ⏱️
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
 
 function HeroSection() {
   const {siteConfig = {}} = useDocusaurusContext();
@@ -39,13 +40,22 @@ function HeroSection() {
   return (
     <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <a href={DK_YT_URL} rel="noopener" target="_blank">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+        </a>
         <img
           alt={siteConfig.title}
           className={clsx(styles.heroBannerLogo, 'margin-vert--md')}
           src={useBaseUrl('img/logo.svg')}
         />
+        <p className="hero__subtitle">
+          
+          Website of the creater of{' '}
+          <a href={ALG_URL} rel="noopener" target="_blank">
+            Arch Linux GUI <br />
+          </a>
+          {siteConfig.tagline} 
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
